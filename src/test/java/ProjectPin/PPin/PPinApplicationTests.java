add2fcfff1f3be89ceae83f9816ccc150dd0db48
JHLEE91 <qpesa1love@gmail.com>
2024-10-01 14:06:16 +0900

    package rebuilding

---
+++ src/test/java/ProjectPin/PPin/PPinApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjectPin.PPin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PPinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
